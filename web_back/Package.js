ce3b1a098389d2f8e8e73a2b63856fa97a91ff34
yuminghui <2404446044@qq.com>
2019-11-24 23:16:10 +0800

    发布完成+获取事件

--- web_back/Package.js
+++ web_back/Package.js
@@ -23,7 +23,7 @@ var Package = {
         })
     },
 
-
+    //————————————————————————————————————————————————————————————————————————————
     //文章类别搜索
     show: function(View) {
         $.get(management.show, {
@@ -63,5 +63,69 @@ var Package = {
         }, function(res) {
             View(res)
         })
+    },
+
+    //——————————————————————————————————————————————————————————————————————————————
+    // 作用：获取第curPage页，类型是tpype,状态是state 的数据 
+    articleShow: function(curPage, type, state, callback) {
+        // 根据接口的定义，要请哪一页的数据，就传对应的值给page
+        $.get(APIURLS.article_show, {
+                page: curPage,
+                type: type,
+                state: state
+            },
+            function(res) {
+                callback(res)
+            })
+    },
+    // 删除文章
+    articleDel: function(id, callback) {
+        $.get(APIURLS.article_del, { 'id': id }, function(res) {
+            callback(res)
+        })
+    },
+    // 添加文章
+    // 参数是formData对象
+    articleAdd: function(fd, callback) {
+        $.ajax({
+            url: APIURLS.article_add,
+            type: 'post',
+            data: fd,
+            processData: false, // 不要让jquery去处理formdata数据
+            contentType: false, // 不要使用默认的请求头
+            success: function(res) {
+                callback(res)
+            }
+        })
+    },
+    // 编辑文章
+    articleEdit: function(fd, callback) {
+        $.ajax({
+            url: APIURLS.article_edit,
+            type: 'post',
+            data: fd,
+            processData: false, // 不要让jquery去处理formdata数据
+            contentType: false, // 不要使用默认的请求头
+            success: function(res) {
+                callback(res)
+            }
+        })
+    },
+    // 获取文章的详情
+    getDetail: function(id, callback) {
+        $.get(APIURLS.article_show, {
+                "id": id
+            },
+            function(res) {
+                callback(res)
+            }
+        )
+    },
+    getMonthCount: function(callback) {
+        $.get(APIURLS.article_month_count, function(res) {
+            callback(res)
+        })
     }
+
+
 }
--- web_back/address.js
+++ web_back/address.js
@@ -6,10 +6,20 @@ var management = {
     methodInfo: address + 'admin/getuser', //改变图片
     methodOut: address + 'admin/logout', // 用户退出
 
+    //------------------------------------------------------------
     show: address + 'admin/category_search', //文章类别搜索
     del: address + 'admin/category_delete', //删除文章类别
     add: address + 'admin/category_add', //新增文章类别
-    edit: address + 'admin/category_edit' //编辑文章类别
+    edit: address + 'admin/category_edit', //编辑文章类别
+
+    //------------------------------------------------------------
+    article_show: address + 'admin/search', //文章搜索
+    article_del: address + 'admin/article_delete', //删除文章
+    article_add: address + 'admin/article_publish', //发布文章
+    article_edit: address + 'admin/article_edit', //文章编辑
+    article_month_count: address + 'admin/month_article_count' // 获取月新增文章数量
+
+    //-------------------------------------------------------------
 
 
 
